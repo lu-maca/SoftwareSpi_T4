fcf312f6f9e5747ce41e3e854981931330108fd9
lucamacavero <luca.macavero@dorbit.space>
2025-05-15 21:26:20 +0200

    resolve conflicts

--- src/SoftwareSpiSlave_T4.cpp
+++ src/SoftwareSpiSlave_T4.cpp
@@ -26,16 +26,19 @@
 
 SoftwareSpiSlave::SoftwareSpiSlave() {}
 
-void SoftwareSpiSlave::begin(const byte sdi_pin, const byte sdo_pin, const byte sck_pin) {
-  static_assert(CPOL < 2, "CPOL shall be 0 or 1");
-  static_assert(CPHA < 2, "CPHA shall be 0 or 1");
+void SoftwareSpiSlave::begin(const byte sdi_pin, const byte sdo_pin,
+                             const byte sck_pin, const byte cpol,
+                             const byte cpha) {
   sdi_pin_ = sdi_pin;
   sck_pin_ = sck_pin;
   sdo_pin_ = sdo_pin;
+  cpol_ = cpol;
+  cpha_ = cpha;
+
   pinMode(sdi_pin_, INPUT);
   pinMode(sdo_pin_, OUTPUT);
   pinMode(sck_pin_, INPUT);
-  // set interrupts priority on all pins
+
   NVIC_SET_PRIORITY(IRQ_GPIO6789, 0);
 }
 
@@ -44,38 +47,58 @@ void SoftwareSpiSlave::add(const byte ss_pin, void (*isr)(void)) {
   attachInterrupt(digitalPinToInterrupt(ss_pin), isr, FALLING);
 }
 
-void SoftwareSpiSlave::byteTransaction(byte* rx, const byte tx, const int ss_pin) const {
-  for (int bit_idx = 0; bit_idx < 8; bit_idx++) {
-    write_sdo(tx, bit_idx);
-    // wait to be ready to read and checking for ss to be asserted too
-    while (wait_for_read() && isActive(ss_pin) )
-      ;
-    // when clock goes HIGH, read the bit on SDI
-    read_sdi(rx, bit_idx);
-    // wait to be ready to write
-    while (wait_for_write())
-      ;
+bool SoftwareSpiSlave::byteTransaction(byte *rx, const byte tx,
+                                       const int ss_pin) const {
+  static const bool edge1 = (cpha_ == 1);
+  static const bool edge2 = (cpha_ == 0);
+  byte sdi_val = 0;
+
+  // Transfer bits 0–6 with helper
+  for (byte i = 0; i < 7; ++i) {
+    if (!transfer_bit(sdi_val, tx, i, edge1, edge2, ss_pin)) {
+      return false;
+    }
   }
-  // this is needed to reset the SDO line after the last bit of the first byte. If
-  // not, the last bit of the first byte is set as first bit of the second byte, since
-  // the master samples at rising clock and rising clock is checked right after this!
-  write_sdo(0, LOW);
+
+  // Final bit 7 (no second edge)
+  if (!isActive(ss_pin)) {
+    return false;
+  }
+  write_sdo(tx, 7);
+  if (!wait_for_edge(edge1, ss_pin)) {
+    return false;
+  }
+  read_sdi(&sdi_val, 7);
+
+  *rx = sdi_val;
+  write_sdo(LOW, 0); // reset SDO
+  return true;
 }
 
-void SoftwareSpiSlave::byteTransaction(volatile byte* rx, const byte tx, const int ss_pin) const {
-  for (int bit_idx = 0; bit_idx < 8; bit_idx++) {
-    write_sdo(tx, bit_idx);
-     // wait to be ready to read and checking for ss to be asserted too
-     while (wait_for_read() && isActive(ss_pin) )
-      ;
-    // when clock goes HIGH, read the bit on SDI
-    read_sdi(rx, bit_idx);
-    // wait to be ready to write
-    while (wait_for_write())
-      ;
+bool SoftwareSpiSlave::byteTransaction(volatile byte *rx, const byte tx,
+                                       const int ss_pin) const {
+  static const bool edge1 = (cpha_ == 1);
+  static const bool edge2 = (cpha_ == 0);
+  byte sdi_val = 0;
+
+  // Transfer bits 0–6 with helper
+  for (byte i = 0; i < 7; ++i) {
+    if (!transfer_bit(sdi_val, tx, i, edge1, edge2, ss_pin)) {
+      return false;
+    }
   }
-  // this is needed to reset the SDO line after the last bit of the first byte. If
-  // not, the last bit of the first byte is set as first bit of the second byte, since
-  // the master samples at rising clock and rising clock is checked right after this!
-  write_sdo(0, LOW);
+
+  // Final bit 7 (no second edge)
+  if (!isActive(ss_pin)) {
+    return false;
+  }
+  write_sdo(tx, 7);
+  if (!wait_for_edge(edge1, ss_pin)) {
+    return false;
+  }
+  read_sdi(&sdi_val, 7);
+
+  *rx = sdi_val;
+  write_sdo(LOW, 0); // reset SDO
+  return true;
 }
--- src/SoftwareSpiSlave_T4.h
+++ src/SoftwareSpiSlave_T4.h
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2017 Adafruit Industries
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,63 +24,80 @@
 
 #ifndef SOFTWARESPISLAVE_T4_H
 #define SOFTWARESPISLAVE_T4_H
+
 #include <Arduino.h>
 
-template <byte CPOL = 0, byte CPHA = 0>
-class SoftwareSpiSlave
-{
- private:
+class SoftwareSpiSlave {
+private:
   byte sdi_pin_;
   byte sdo_pin_;
   byte sck_pin_;
+  byte cpha_;
+  byte cpol_;
 
- protected:
-  // return `true` if we shall wait for reading the SDI
-  inline const bool wait_for_read() const { 
-    if constexpr(CPOL == CPHA) {
-      return (digitalReadFast(sck_pin_) == LOW); 
-    } else if constexpr(CPOL != CPHA) {
-      return (digitalReadFast(sck_pin_) == HIGH); 
+protected:
+  // Edge detection: wait for specified edge
+  inline bool wait_for_edge(bool rising, byte ss_pin) const {
+    bool prev = digitalReadFast(sck_pin_);
+    while (true) {
+      bool curr = digitalReadFast(sck_pin_);
+      if (rising && !prev && curr)
+        return true; // Rising edge
+      if (!rising && prev && !curr)
+        return true; // Falling edge
+      if (!isActive(ss_pin))
+        return false;
+      prev = curr;
     }
   }
-  // return `true` if we shall wait for writing on the SDO
-  inline const bool wait_for_write() const { 
-    if constexpr(CPOL == CPHA) {
-      return (digitalReadFast(sck_pin_) == HIGH); 
-    } else if constexpr(CPOL != CPHA) {
-      return (digitalReadFast(sck_pin_) == LOW); 
-    }
+
+  inline const bool is_clock_high() const {
+    return (digitalReadFast(sck_pin_) == HIGH);
   }
-  // read `bit_idx`-th bit on SDI line, store it in `rcv` `bit_idx`-th bit; note: bit endianness!
-  inline void read_sdi(volatile byte* rcv, const byte bit_idx) const
-  {
+
+  // Read/write helpers
+  inline void read_sdi(volatile byte *rcv, const byte bit_idx) const {
     bitWrite(*rcv, 7 - bit_idx, digitalReadFast(sdi_pin_));
   }
-  // read `bit_idx`-th bit on SDI line, store it in `rcv` `bit_idx`-th bit; note: bit endianness!
-  inline void read_sdi(byte* rcv, const byte bit_idx) const
-  {
+
+  inline void read_sdi(byte *rcv, const byte bit_idx) const {
     bitWrite(*rcv, 7 - bit_idx, digitalReadFast(sdi_pin_));
   }
-  // write `bit_idx`-th bit of `val` on SDO line; note: bit endianness!
-  inline void write_sdo(const byte val, const byte bit_idx) const
-  {
+
+  inline void write_sdo(const byte val, const byte bit_idx) const {
     digitalWriteFast(sdo_pin_, bitRead(val, 7 - bit_idx));
   }
 
- public:
-  // constructor
-  SoftwareSpiSlave();
-  // add slave select on pin `ss_pin`, with handler `isr`
-  void add(byte ss_pin, void (*isr)(void));
-  // begin spi, call this after adding all slaves
-  void begin(const byte sdi_pin, const byte sdo_pin, const byte sck_pin);
-  // perform a transaction of a single byte
-  void byteTransaction(byte* rx, const byte tx, const int ss_pin) const;
-  // perform a transaction of a single byte
-  void byteTransaction(volatile byte* rx, const byte tx, const int ss_pin) const;
-  // check if specified slave select is asserted
-  const bool isActive(const byte ss) const { return (digitalReadFast(ss) == LOW); }
+  inline bool transfer_bit(byte &sdi_val, const byte tx, const byte bit_idx,
+                           const bool edge1, const bool edge2,
+                           const byte ss_pin) const {
+    if (!isActive(ss_pin))
+      return false;
+    write_sdo(tx, bit_idx);
+    if (!wait_for_edge(edge1, ss_pin))
+      return false;
+    read_sdi(&sdi_val, bit_idx);
+    if (!wait_for_edge(edge2, ss_pin))
+      return false;
+    return true;
+  }
 
+public:
+  SoftwareSpiSlave();
+
+  void add(byte ss_pin, void (*isr)(void));
+
+  void begin(const byte sdi_pin, const byte sdo_pin, const byte sck_pin,
+             const byte cpol, const byte cpha);
+
+  bool byteTransaction(byte *rx, const byte tx, const int ss_pin) const;
+
+  bool byteTransaction(volatile byte *rx, const byte tx,
+                       const int ss_pin) const;
+
+  const bool isActive(const byte ss) const {
+    return (digitalReadFast(ss) == LOW);
+  }
 };
 
 #endif
