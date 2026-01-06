4c0809a16f9cb333225fc5c45979df4dc661881a
lucamacavero <luca.macavero@gmail.com>
2026-01-06 21:56:50 +0100

    fix spi slave for all modes

--- src/SoftwareSpiSlave_T4.cpp
+++ src/SoftwareSpiSlave_T4.cpp
@@ -1,51 +1,54 @@
 
 #include "SoftwareSpiSlave_T4.h"
 
-#define BYTE_TRANSACTION                                 \
-  static const bool mode02 = (mode_ == 0 || mode_ == 2); \
-  static const bool mode01 = (mode_ == 0 || mode_ == 1); \
-  static const bool mode13 = (mode_ == 1 || mode_ == 3); \
-  for (int bit_idx = 0; bit_idx < 8; bit_idx++)          \
-  {                                                      \
-    if (mode02)                                          \
-    {                                                    \
-      write_sdo(tx, bit_idx);                            \
-    }                                                    \
-    if (mode_ == 0 || mode_ == 1)                        \
-    {                                                    \
-      while (!is_clock_high() && isActive(ss_pin))       \
-        ;                                                \
-    }                                                    \
-    else                                                 \
-    {                                                    \
-      while (is_clock_high() && isActive(ss_pin))        \
-        ;                                                \
-    }                                                    \
-    if (mode02)                                          \
-    {                                                    \
-      read_sdi(rx, bit_idx);                             \
-    }                                                    \
-    else                                                 \
-    {                                                    \
-      write_sdo(tx, bit_idx);                            \
-    }                                                    \
-    if (mode01)                                          \
-    {                                                    \
-      while (is_clock_high())                            \
-        ;                                                \
-    }                                                    \
-    else                                                 \
-    {                                                    \
-      while (!is_clock_high())                           \
-        ;                                                \
-    }                                                    \
-    if (mode13)                                          \
-    {                                                    \
-      read_sdi(rx, bit_idx);                             \
-    }                                                    \
+#define BYTE_TRANSACTION                           \
+  for (int bit_idx = 0; bit_idx < 8; bit_idx++)    \
+  {                                                \
+    if (mode02_)                                   \
+    {                                              \
+      write_sdo(tx, bit_idx);                      \
+    }                                              \
+    if (mode01_)                                   \
+    {                                              \
+      while (!is_clock_high() && isActive(ss_pin)) \
+        ;                                          \
+    }                                              \
+    else                                           \
+    {                                              \
+      while (is_clock_high() && isActive(ss_pin))  \
+        ;                                          \
+    }                                              \
+    if (mode02_)                                   \
+    {                                              \
+      read_sdi(rx, bit_idx);                       \
+    }                                              \
+    else                                           \
+    {                                              \
+      write_sdo(tx, bit_idx);                      \
+    }                                              \
+    if (mode01_)                                   \
+    {                                              \
+      while (is_clock_high() && isActive(ss_pin))  \
+        ;                                          \
+    }                                              \
+    else                                           \
+    {                                              \
+      while (!is_clock_high() && isActive(ss_pin)) \
+        ;                                          \
+    }                                              \
+    if (_)                                         \
+    {                                              \
+      read_sdi(rx, bit_idx);                       \
+    }                                              \
   }
 
-SoftwareSpiSlave::SoftwareSpiSlave(const byte cpol, const byte cpha) : mode_(cpha | cpol) {}
+SoftwareSpiSlave::SoftwareSpiSlave(const SpiMode mode)
+{
+  const byte mode = static_cast<byte>(mode);
+  mode02_ = (mode == 0 || mode == 2);
+  mode01_ = (mode == 0 || mode == 1);
+  mode13_ = (mode == 1 || mode == 3);
+}
 
 void SoftwareSpiSlave::begin(const byte sdi_pin, const byte sdo_pin, const byte sck_pin)
 {
--- src/SoftwareSpiSlave_T4.h
+++ src/SoftwareSpiSlave_T4.h
@@ -3,24 +3,35 @@
 #define SOFTWARESPISLAVE_T4_H
 #include <Arduino.h>
 
+enum class SpiMode : byte
+{
+  _0 = 0,
+  _1 = 1,
+  _2 = 2,
+  _3 = 3
+};
+
 class SoftwareSpiSlave
 {
- private:
+private:
+  const byte mode_;
   byte sdi_pin_;
   byte sdo_pin_;
   byte sck_pin_;
-  const byte mode_;
+  bool mode02_;
+  bool mode01_;
+  bool mode13_;
 
- protected:
+protected:
   // return `true` if clock is HIGH
   inline const bool is_clock_high() const { return (digitalReadFast(sck_pin_) == HIGH); }
   // read `bit_idx`-th bit on SDI line, store it in `rcv` `bit_idx`-th bit; note: bit endianness!
-  inline void read_sdi(volatile byte* rcv, const byte bit_idx) const
+  inline void read_sdi(volatile byte *rcv, const byte bit_idx) const
   {
     bitWrite(*rcv, 7 - bit_idx, digitalReadFast(sdi_pin_));
   }
   // read `bit_idx`-th bit on SDI line, store it in `rcv` `bit_idx`-th bit; note: bit endianness!
-  inline void read_sdi(byte* rcv, const byte bit_idx) const
+  inline void read_sdi(byte *rcv, const byte bit_idx) const
   {
     bitWrite(*rcv, 7 - bit_idx, digitalReadFast(sdi_pin_));
   }
@@ -30,20 +41,19 @@ class SoftwareSpiSlave
     digitalWriteFast(sdo_pin_, bitRead(val, 7 - bit_idx));
   }
 
- public:
+public:
   // constructor
-  SoftwareSpiSlave(const byte cpol, const byte cpha);
+  SoftwareSpiSlave(const SpiMode mode);
   // add slave select on pin `ss_pin`, with handler `isr`
   void add(byte ss_pin, void (*isr)(void));
   // begin spi, call this after adding all slaves
   void begin(const byte sdi_pin, const byte sdo_pin, const byte sck_pin);
   // perform a transaction of a single byte
-  void byteTransaction(byte* rx, const byte tx, const int ss_pin) const;
+  void byteTransaction(byte *rx, const byte tx, const int ss_pin) const;
   // perform a transaction of a single byte
-  void byteTransaction(volatile byte* rx, const byte tx, const int ss_pin) const;
+  void byteTransaction(volatile byte *rx, const byte tx, const int ss_pin) const;
   // check if specified slave select is asserted
   const bool isActive(const byte ss) const { return (digitalReadFast(ss) == LOW); }
-
 };
 
 #endif
